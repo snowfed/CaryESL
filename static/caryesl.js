7328ad113cf6a5297d5c0b1354d20017f5c30e8e
Fedor Anikeev <snowfed@gmail.com>
2018-01-02 14:01:45 +0300

    Added a mobile menu for vertical screens.

--- static/caryesl.js
+++ static/caryesl.js
@@ -1,29 +1,48 @@
+function open_nav() {
+	$("#main_side_nav").css("width", "250px");
+}
+
+function close_nav() {
+	$("#main_side_nav").css("width", "0px");
+}
+
+function open_section (nav_item)
+{
+	var main_section = $("#main_section");
+	var item = nav_item.replace("_mobile", "");
+	console.log(item);
+	switch (item) {
+		case "about":
+			main_section.html("<h1>Who is George?</h1><img src='/static/caryesl_students.jpg'>");
+			break;
+		case "material":
+			main_section.html("<h1>George can teach you A LOT!</h1>");
+			break;
+		case "timetable":
+			main_section.html("<h1>George has no time to teach you.</h1>");
+			break;
+		case "contacts":
+			main_section.html("<h1>How can I reach George?</h1><a href=\"mailto:georgemartin11@hotmail.com?subject=Feedback\">georgemartin11@hotmail.com</a>");
+			break;
+		case "promo":
+			main_section.html("<h1>No promotional offers at the moment...</h1>");
+			break;
+		case "test":
+			main_section.html("<h1>I can see that your English is bad. Study with me!</h1>");
+			break;
+	}
+}
+
 $(function() {
 	$("li a").click(function() {
 		$("li a").removeClass("active");
 		$(this).addClass("active");
 		var nav_item = $(this).attr("id");
-		var main_section = $("#main_section");
-		switch (nav_item) {
-			case "London":
-				main_section.html("<h1>London</h1>")
-				break;
-			case "Paris":
-				main_section.html("<h1>Paris</h1>")
-				break;
-			case "Tokyo":
-				main_section.html("<h1>Tokyo</h1>")
-				break;
-			case "Washington":
-				main_section.html("<img src='/static/caryesl_students.jpg'>");
-				break;
-			case "Moscow":
-				main_section.html("<h1>Moscow</h1>")
-				break;
-			case "Canberra":
-				main_section.html("<h1>Canberra</h1>")
-				break;
-		}
+		open_section(nav_item);
+	});
+	$("#main_side_nav a").click(function() {
+		var nav_item = $(this).attr("id");
+		open_section(nav_item);
 	});
 	$("#main_header").click(function() {
 		$("li a").removeClass("active");
