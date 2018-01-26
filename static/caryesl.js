e30cdad1989be701c68050ba5afbf460a333cd8b
Fedor Anikeev <snowfed@gmail.com>
2018-01-26 22:00:37 +0300

    Merged two menus into one and added the promo page.

--- static/caryesl.js
+++ static/caryesl.js
@@ -1,11 +1,11 @@
 function open_nav ()
 {
-	$("#main_side_nav").css("width", "60vw");
+	$("div.sidenav").addClass("open");
 }
 
 function close_nav ()
 {
-	$("#main_side_nav").css("width", "0vw");
+	$("div.sidenav").removeClass("open");
 }
 
 function switch_language ()
@@ -15,68 +15,28 @@ function switch_language ()
 	$("button.language").text(main_section.hasClass("Russian") ? "ENG" : "RUS");
 	$("div.Russian").toggle();
 	$("div.English").toggle();
-	var active_page = $("li a.active").attr("id");
+	var active_page = $("nav a.active").attr("id");
 	if (active_page == undefined)
 		return;
-	open_section(active_page);
+	open_particular_section(active_page);
 }
 
-function open_particular_section (item, page_name, main_section = null)
+function open_particular_section (item)
 {
-	if (main_section == null)
-		main_section = $("#main_section");
+	var main_section = $("#main_section");
 	var ending = (main_section.hasClass("Russian") ? "_rus.html" : ".html");
-	main_section.load("static/" + page_name + ending);
-}
-
-function open_section (nav_item)
-{
-	var item = nav_item.replace("_mobile", "");
-	switch (item) {
-		case "about":
-			open_particular_section(item, "about");
-			break;
-		case "material":
-			open_particular_section(item, "material");
-			break;
-		case "timetable":
-			open_particular_section(item, "timetable");
-			break;
-		case "contacts":
-			open_particular_section(item, "contacts");
-			break;
-		case "promo":
-			$("#main_section").html("<h1>No promotional offers at the moment...</h1>");
-			break;
-		case "test":
-			open_particular_section(item, "test");
-			break;
-		default:
-			console.log("Unexpected item in open_section():", item);
-			item = null;
-	}
-	return item;
+	main_section.load("static/" + item + ending);
 }
 
 $(function() {
-	$("li a").click(function() {
-		$("li a").removeClass("active");
+	$("nav a").click(function() {
+		$("nav a").removeClass("active");
 		$(this).addClass("active");
 		var nav_item = $(this).attr("id");
-		open_section(nav_item);
+		open_particular_section(nav_item);
+		close_nav();
 	});
-	$("#main_side_nav a").click(function() {
-		if ($(this).hasClass("closebtn")) {
-			close_nav();
-		} else {
-			var item = open_section($(this).attr("id"));
-			if (item != null) {
-				$("li a").removeClass("active");
-				$("#"+item).addClass("active");
-				close_nav();
-			}
-		}
-	});
+	$("a.closebtn").click(close_nav);
 	$("div.header").click(function() {
 		$("li a").removeClass("active");
 		var main_section = $("#main_section");
